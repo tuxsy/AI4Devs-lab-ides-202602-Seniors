b346b2cd089a71bc6ddd60c9d9a3b00c98435f0d
Bru Anglés Robles <bangles@gmail.com>
2026-03-16 17:02:11 +0100

    chore: export the openapi specification

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -224,6 +224,10 @@ const swaggerSpec = swaggerJsdoc({
 });
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.get('/api-docs.json', (_req: Request, res: Response) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(swaggerSpec);
+});
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hola LTI!');
